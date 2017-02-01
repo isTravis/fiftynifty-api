8f750728914f225be05da52e892dc8c5ff637a1c
Travis Rich <travis.s.rich@gmail.com>
2017-02-01 13:05:52 -0500

    Fix encryption bug with word array

--- utilities/encryption.js
+++ utilities/encryption.js
@@ -1,16 +1,15 @@
 import CryptoJS from 'crypto-js';
 
 export function encryptPhone(phone) {	
-	const secret = process.env.PHONE_KEY;
+	const secret = CryptoJS.enc.Utf8.parse(process.env.PHONE_KEY);
 	const salt = CryptoJS.enc.Base64.parse(process.env.PHONE_SALT);
 	const iv = CryptoJS.enc.Base64.parse(process.env.PHONE_IV);
-
 
 	return CryptoJS.AES.encrypt(phone, secret, { salt: salt, iv: iv }).toString();
 }
 
 export function decryptPhone(cipherText) {	
-	const secret = process.env.PHONE_KEY;
+	const secret = CryptoJS.enc.Utf8.parse(process.env.PHONE_KEY);
 	const salt = CryptoJS.enc.Base64.parse(process.env.PHONE_SALT);
 	const iv = CryptoJS.enc.Base64.parse(process.env.PHONE_IV);
 
