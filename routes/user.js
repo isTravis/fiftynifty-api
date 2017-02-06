b85695e60829f514d0e4d0ef13e6d840ce462c97
Travis Rich <travis.s.rich@gmail.com>
2017-02-06 06:39:05 -0500

    Cleaning up and restructuring user.js a bit

--- routes/user.js
+++ routes/user.js
@@ -7,17 +7,16 @@ import { encryptPhone } from '../utilities/encryption';
 export const userAttributes = ['id', 'name', 'zipcode', 'parentId', 'hierarchyLevel', 'lat', 'lon', 'createdAt', 'state', 'district'];
 
 const getStateDistrict = function (locData) {
-    const lookupQuery = locData.lat ? `latitude=${locData.lat}&longitude=${locData.lon}` : `zip=${locData.zipcode}`;
-    const apiRequestUrl = `https://congress.api.sunlightfoundation.com/districts/locate?apikey=${process.env.SUNLIGHT_FOUNDATION_KEY}&${lookupQuery}`;
-    const getStateDist = request({uri: apiRequestUrl, json: true} )
-        .then((response) => {
-            return response.results && response.results[0] || {'state': null, 'district':null};
-        })
-        .catch((err) => {
-            console.log(err);
-            return {'state': null, 'district':null};
-        });
-    return getStateDist;
+	const lookupQuery = locData.lat ? `latitude=${locData.lat}&longitude=${locData.lon}` : `zip=${locData.zipcode}`;
+	const apiRequestUrl = `https://congress.api.sunlightfoundation.com/districts/locate?apikey=${process.env.SUNLIGHT_FOUNDATION_KEY}&${lookupQuery}`;
+	return request({ uri: apiRequestUrl, json: true })
+	.then((response) => {
+		return (response.results && response.results[0]) || { state: null, district: null };
+	})
+	.catch((err) => {
+		console.log(err);
+		return { state: null, district: null };
+	});
 };
 
 const queryForUser = function(userId) {
@@ -58,32 +57,33 @@ app.get('/user', getUser);
 
 export function postUser(req, res, next) {	
 	const phoneHash = encryptPhone(req.body.phone);
-	const locData = { zipcode :req.body.zipcode};
-    getStateDistrict(locData).then((stateDist)=>{
-		User.create({
+	const locData = { zipcode: req.body.zipcode };
+	getStateDistrict(locData)
+	.then(function(stateDist) {
+		return User.create({
 			phone: phoneHash,
 			name: req.body.name,
 			zipcode: req.body.zipcode,
 			parentId: req.body.parentId,
 			state: stateDist.state,
 			district: stateDist.district,
-		})
-		.then(function(result) {
-			return User.find({
-				where: {
-					id: result.id
-				},
-				attributes: userAttributes
-			});
-		})
-		.then(function(userData) {
-			return res.status(201).json(userData);
-		})
-		.catch(function(err) {
-			console.error('Error in postUser: ', err);
-			return res.status(500).json('Phone number already used');
 		});
-    });
+	})
+	.then(function(result) {
+		return User.find({
+			where: {
+				id: result.id
+			},
+			attributes: userAttributes
+		});
+	})
+	.then(function(userData) {
+		return res.status(201).json(userData);
+	})
+	.catch(function(err) {
+		console.error('Error in postUser: ', err);
+		return res.status(500).json('Phone number already used');
+	});
 }
 app.post('/user', postUser);
 
@@ -105,16 +105,18 @@ export function findLatLocFromAddressInput(req, res, next) {
 		}, '00000');
 
 		if (zip === '00000') { console.log(apiRequestUrl); }
-        const locData = { lat :lat, lon:lon};
-        getStateDistrict(locData).then((response)=>{
-				const state = response.state;
-				const district = response.district;
-				return User.update({ lat: lat, lon: lon, zip: zip, state:state, district:district }, {
-					where: {
-						id: req.body.userId
-					}
-				});
-		})
+		const locData = { lat: lat, lon: lon };
+		
+		return Promise.all([lat, lon, zip, getStateDistrict(locData)]);
+	})
+	.spread(function(lat, lon, zip, getLocResult) {
+		const state = getLocResult.state;
+		const district = getLocResult.district;
+		return User.update({ lat: lat, lon: lon, zip: zip, state: state, district: district }, {
+			where: {
+				id: req.body.userId
+			}
+		});
 	})
 	.then(function(updateCount) {
 		return queryForUser(req.body.userId);
