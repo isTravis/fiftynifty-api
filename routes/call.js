8e7dd7f1a33e030606027510fc3cac8c020a6281
lemeb <lemebfr@gmail.com>
2017-02-11 12:58:05 -0500

    Add call status change for both sides

--- routes/call.js
+++ routes/call.js
@@ -4,31 +4,30 @@ import { User, Call } from '../models';
 import { encryptPhone, decryptPhone } from '../utilities/encryption';
 import { queryForUser } from './user';
 import request from 'request-promise';
+import { parse, format } from 'libphonenumber-js';
 
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const urldomain = process.env.API_SERVER;
 
 export function callFromServer(req, res) {
-	const userId = req.body.userId;
+	const id = req.body.id;
 	const repId = req.body.repId;
-	console.log(`Call from frontend - user #${userId} to ${repId}`);
+	console.log(`Call from frontend - user #${id} to ${repId}`);
 
 	User.findOne({
 		where: {
-			id: userId,
+			id: id,
 		}
 	})
 	.then(function(newUser) {
 		const userPhone = decryptPhone(newUser.dataValues.phone);
-		console.log(`Call from frontend - phone ${userPhone} to ${repId}`);
 		const urlToCall = `${urldomain}/newcall/${repId}/`;
-		console.log(urlToCall);
 		client.makeCall({
 			to: userPhone,
 			from: process.env.TWILIO_NUMBER,
 			url: urlToCall,
+			StatusCallback: `${urldomain}/callStatusChange`,
 		}, function (err, message) {
-			console.log(err);
 			if (err) {
 				res.status(500).send(err);
 			} else {
@@ -42,11 +41,21 @@ export function callFromServer(req, res) {
 app.post('/callfromserver', callFromServer);
 
 export function newCall(req, res, next) {	
-	console.log('New call', req.body);
+	// console.log('New call', req.body);
 	const call = new twilio.TwimlResponse();
 	const fromServer = req.body.From === process.env.TWILIO_NUMBER ? true : false;
 	const userPhone = fromServer ? req.body.To : req.body.From;
 	const connectingToRep  = function(call, repData){
+		const repPhone = format(parse(repData.phone, 'US'), 'International_plaintext');
+		Call.create({
+				twilioId: req.body.CallSid,
+				numberDialed: repPhone,
+				recipientId: repData.bioguide_id,
+				district: ( repData.title === 'Sen' ) ? 0 : repData.district,
+				state: repData.state,
+				zip: '00000',
+				completed: 0,
+		});
 		const name = repData.first_name + ' ' + repData.last_name;
 		call.say(`Connecting to ${name}`);
 		call.dial({ hangupOnStar: true }, process.env.ANDY_NUMBER);
@@ -57,17 +66,15 @@ export function newCall(req, res, next) {
 	};
 	if (fromServer) {
 		const repId = req.params.repId;
-		console.log('hello');
 		return request({
 			uri: `https://congress.api.sunlightfoundation.com/legislators?apikey=${process.env.SUNLIGHT_FOUNDATION_KEY}&bioguide_id=${repId}`, 
 			json: true,
 		})
 		.then(function(repData) {
-			console.log(repData);
 			connectingToRep(call, repData.results[0]);
 		});
 	} else {
-		return queryForUser(userPhone)
+		return queryForUser(userPhone, 'phone')
 		.then(function(result) {
 			connectingToRep(call, result.reps[0]);
 		})
@@ -89,19 +96,25 @@ app.post('/newcall', newCall);
 app.post('/newcall/:repId', newCall);
 
 export function callStatusChange(req, res, next) {	
-	console.log('In call status change ', req.body);
+	// console.log('In call status change ', req.body);
+	const fromServer = req.body.From === process.env.TWILIO_NUMBER ? true : false;
+	const userPhone = fromServer ? req.body.To : req.body.From;
 	if (req.body.CallStatus === 'completed') {
 		return User.findOne({
 			where: {
-				phone: encryptPhone(req.body.From),
+				phone: encryptPhone(userPhone),
 			}
 		})
 		.then(function(callingUser) {
-			return Call.create({
-				numberDialed: req.body.To,
-				state: req.body.ToState, // This is not the real state value we want, we want the reps state and district.
+			return Call.update({
 				duration: req.body.CallDuration,
 				callerId: callingUser.id,
+				zipcode: callingUser.zipcode,
+				completed: 1,
+			}, {
+				where: {
+					twilioId: req.body.CallSid,
+				}
 			});
 		})
 		.then(function() {
