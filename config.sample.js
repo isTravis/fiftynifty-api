97f118f7116b9d01014fe8c981b6f7e9a83c7f13
Travis Rich <travis.s.rich@gmail.com>
2017-03-10 16:59:26 -0500

    Add back config.js

---
+++ config.sample.js
@@ -0,0 +1,20 @@
+process.env.DATABASE_URL = '<DATABASE_URL>';
+process.env.REDIS_URL = '<REDIS_URL>';
+
+process.env.OLD_PHONE_KEY = '<OLD_PHONE_KEY>';
+process.env.OLD_PHONE_SALT = '<OLD_PHONE_SALT>';
+process.env.OLD_PHONE_IV = '<OLD_PHONE_IV>';
+
+process.env.PHONE_KEY = '<PHONE_KEY>';
+process.env.PHONE_SALT = '<PHONE_SALT>';
+process.env.PHONE_IV = '<PHONE_IV>';
+
+process.env.TWILIO_AUTH_TOKEN = '<TWILIO_AUTH_TOKEN>';
+process.env.TWILIO_ACCOUNT_SID = '<TWILIO_ACCOUNT_SID>';
+process.env.TWILIO_NUMBER = '<TWILIO_NUMBER>';
+
+process.env.API_SERVER = '<API_SERVER>';
+process.env.GEOCODING_API_KEY = '<GEOCODING_API_KEY>';
+process.env.SUNLIGHT_FOUNDATION_KEY = '<SUNLIGHT_FOUNDATION_KEY>';
+process.env.ANDY_NUMBER = '<ANDY_NUMBER>';
+
